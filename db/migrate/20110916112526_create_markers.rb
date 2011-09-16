3a82c60d375579081718d227470d10c8d6358fca
pascal <pascal@pascal-VirtualBox.(none)>
2011-09-16 15:31:06 +0600

    added group and marker models

---
+++ db/migrate/20110916112526_create_markers.rb
@@ -0,0 +1,18 @@
+class CreateMarkers < ActiveRecord::Migration
+  def self.up
+    create_table :markers do |t|
+      t.string :title
+      t.text :description
+      t.string :address
+      t.float :longitude
+      t.float :latitude
+      t.boolean :gmaps
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :markers
+  end
+end
