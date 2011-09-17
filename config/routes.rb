e16408670d47cc1b0508c5c5832361939e4869c1
ZuevEvgenii <zuev.evgenii@gmail.com>
2011-09-17 20:33:33 +0600

    Some fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Vitiligo::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  root :to => "countries#index"
+  root :to => "groups#index"
 
   resources :countries
   resources :groups, :only => [:index, :show]
