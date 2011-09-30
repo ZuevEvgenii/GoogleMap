3d9843701c0e6f47dadb2f3754a5b20d2f5d221a
ZuevEvgenii <zuev.evgenii@gmail.com>
2011-09-30 10:56:16 +0600

    Modified marker creation

--- app/uploaders/marker_icon_uploader.rb
+++ app/uploaders/marker_icon_uploader.rb
@@ -36,6 +36,10 @@ class MarkerIconUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [40, 40]
   end
 
+  version :marker_preview do
+    process :resize_to_limit => [95, 70]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
