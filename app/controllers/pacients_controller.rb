fea85c5e47ba2b1b25d3c4f2f5edf0511940bd22
ZuevEvgenii <zuev.evgenii@gmail.com>
2011-11-01 14:41:55 +0500

    Fixed registration

--- app/controllers/pacients_controller.rb
+++ app/controllers/pacients_controller.rb
@@ -1,13 +1,17 @@
 class PacientsController < ApplicationController
   def register
-    @user = User.new(params[:user])
-    if @user.valid?
-      @user.activated = false
-      @user.marker = Marker.new(:address => params[:user][:location], :group => Group.find_by_title("Pacients"), :name => params[:user][:name])
-    end
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    @user.marker = Marker.new(:address => params[:user][:location], :group => Group.find_by_title("Patients"), :name => params[:user][:name])
+    @user.activated = false
+
 
     if @user.save
-      render :js => "$('#dialog').hide();"
+      render :js => "$('#dialog').parent().hide();"
+    else
+      render :js => "$('#dialog_errors').html('Not valid user');"
     end
   end
 end
