f38d5b9566d4ed5155984473dc17ad8d353c87dc
ZuevEvgenii <zuev.evgenii@gmail.com>
2011-11-11 10:28:25 +0600

    Migration

---
+++ db/migrate/20111111041035_add_description_to_groups.rb
@@ -0,0 +1,10 @@
+class AddDescriptionToGroups < ActiveRecord::Migration
+  def self.up
+    add_column :groups, :description, :text
+  end
+
+  def self.down
+    remove_column :groups, :description
+  end
+end
+
